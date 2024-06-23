901c6353c1b61f7ed975ed348e1ee9d86d4cf27c
Antton Alberdi <37664231+anttonalberdi@users.noreply.github.com>
2024-06-23 07:37:17 +0200

    Update run_dram.sh

--- run_dram.sh
+++ run_dram.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=%s                   # Job name
+
+$1
+
+#SBATCH --job-name=${1}
 #SBATCH --nodes=1
-#SBATCH --ntasks=4                      # Run on 4 CPUs
-#SBATCH --mail-user=antton.alberdi@sund.ku.dk
-#SBATCH --mem=200gb                     # Job memory request
-#SBATCH --time=%d                       # In minutes
+#SBATCH --ntasks=24
+#SBATCH --mem=64gb
+#SBATCH --time=600 
 
 module load mamba/1.3.1
 source activate /projects/mjolnir1/people/ncl550/0_software/miniconda3/envs/DRAM_more_modules
 
-DRAM.py annotate \-i {input.mag} \-o {params.outdir} \--threads 24 --min_contig_size 1500 
+DRAM.py annotate -i ${1}.fasta -o dram/${1} --threads 24 --min_contig_size 1500 
